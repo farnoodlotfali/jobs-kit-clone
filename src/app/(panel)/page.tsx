a0066445be0c90289c1954cdb3f4b5fd93663707
farnoodlotfali <farnoodlotfali@gmail.com>
2025-02-07 15:55:07 +0330

    fix bug

--- src/app/(panel)/page.tsx
+++ src/app/(panel)/page.tsx
@@ -1,17 +1,21 @@
 import { Suspense } from "react";
 import { Metadata } from "next";
+import { cookies } from "next/headers";
 
 import LoadingSpinner from "@/components/LoadingSpinner";
 import JobsListScreen from "@/components/screens/job-list";
+import { USER_TOKEN } from "@/constants/storage";
 
 export const metadata: Metadata = {
   title: "جابزکیت - شغل‌ها",
 };
 
 const JobsList = async () => {
+  const token = (await cookies()).get(USER_TOKEN);
+
   return (
     <Suspense fallback={<LoadingSpinner />}>
-      <JobsListScreen />
+      <JobsListScreen hasUser={!!token} />
     </Suspense>
   );
 };
