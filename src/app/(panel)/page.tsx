6682f177e3d3c15e6d69653daaa0251b5c096ade
farnoodlotfali <farnoodlotfali@gmail.com>
2025-02-07 13:57:12 +0330

    client side

--- src/app/(panel)/page.tsx
+++ src/app/(panel)/page.tsx
@@ -1,24 +1,27 @@
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+// import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Metadata } from "next";
 
 import JobsListScreen from "@/components/screens/job-list";
-import { prefetchInfiniteJobs } from "@/hooks/queries";
-import { PJobFilter } from "@/types/api/job";
+
+// import { prefetchInfiniteJobs } from "@/hooks/queries";
+// import { PJobFilter } from "@/types/api/job";
 
 export const metadata: Metadata = {
   title: "جابزکیت - شغل‌ها",
 };
 
-type PageProps = {
-  searchParams: PJobFilter;
-};
+// type PageProps = {
+//   searchParams: PJobFilter;
+// };
 
-const JobsList = async ({ searchParams }: PageProps) => {
-  return (
-    // <HydrationBoundary state={dehydrate(await prefetchInfiniteJobs(searchParams))}>
-    <JobsListScreen />
-    // </HydrationBoundary>
-  );
-};
+const JobsList = async () =>
+  // { searchParams }: PageProps
+  {
+    return (
+      // <HydrationBoundary state={dehydrate(await prefetchInfiniteJobs(searchParams))}>
+      <JobsListScreen />
+      // </HydrationBoundary>
+    );
+  };
 
 export default JobsList;
